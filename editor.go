86a4324628987ef0b911d79aee47edd36eb2dc7e
Stephen Hung <hung.stephen.yh@gmail.com>
2020-12-04 14:51:18 -0800

    Seperate text and uid for clients

--- editor.go
+++ editor.go
@@ -6,6 +6,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 // Text Body - full text currently in Quill editor
@@ -35,14 +37,32 @@ type Page struct {
 }
 
 var currentText string = ""
+var currentID int = 0
+var userTextDir = make(map[int]string)
 
 // can refactor away, currently working..
 func loadPage() (*Page, error) {
 	return &Page{Body: currentText}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("uid")
+	if err != nil {
+		http.SetCookie(w, &http.Cookie{
+			Name:    "uid",
+			Value:   strconv.Itoa(currentID),
+			Expires: time.Now().Add(120 * time.Second),
+		})
+		userTextDir[currentID] = ""
+		currentID++
+		fmt.Println("Setting current client uid to : ", currentID)
+	}
+	http.Redirect(w, r, "/editor", http.StatusFound)
+}
+
+func editorHandler(w http.ResponseWriter, r *http.Request) {
 	// title := r.URL.Path[len("/edit/"):]
+
 	p, err := loadPage()
 	if err != nil {
 		fmt.Println("Error loading page")
@@ -56,6 +76,16 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	var currentBody TextBody
 	var currentHistory TextHistory
 
+	cookie, err := r.Cookie("uid")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cookie)
+	currUID, uidErr := strconv.Atoi(cookie.Value)
+	if uidErr != nil {
+		log.Fatal(uidErr)
+	}
+
 	// get form value for body and history
 	body := r.FormValue("body")
 	history := r.FormValue("history")
@@ -68,20 +98,29 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal([]byte(history), &currentHistory)
 
 	// --- sample print of unmarshelled json
-	fmt.Printf("Insert: %s", currentBody.Ops[0].Insert)
-	for i, s := range currentHistory.Ops {
-		fmt.Println(i, s)
+	for _, ele := range currentBody.Ops {
+		fmt.Println(ele)
+	}
+	if len(currentBody.Ops) != 0 {
+		fmt.Printf("Insert: %s", currentBody.Ops[0].Insert)
+		for i, s := range currentHistory.Ops {
+			fmt.Println(i, s)
+		}
+
+		// --- workaround for reset page after form submit
+		currentText = currentBody.Ops[0].Insert
+		userTextDir[currUID] = currentText
+	} else {
+		currentText = userTextDir[currUID]
 	}
 
-	// --- workaround for reset page after form submit
-	currentText = currentBody.Ops[0].Insert
-
 	// redirect back to edit page
-	http.Redirect(w, r, "/", http.StatusFound)
+	http.Redirect(w, r, "/editor", http.StatusFound)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/editor", editorHandler)
 	http.HandleFunc("/save", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
