543d4c70567e09b610c607c388f6f72f79c838dd
Stephen Hung <hung.stephen.yh@gmail.com>
2020-12-06 17:46:21 -0800

    finished remote and local sync + update text

--- editor.go
+++ editor.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 
 	//	"time"
 	socketio "github.com/googollee/go-socket.io"
@@ -16,21 +17,10 @@ type TextBody struct {
 	Ops []Operation
 }
 
-// Text History - a history of changes
-type TextHistory struct {
-	Ops []OperData
-}
-
 type Operation struct {
 	Insert string
 	Delete int
 	Retain int
-}
-
-type OperData struct {
-	Position int
-	Type     string
-	Data     string
 }
 
 type Page struct {
@@ -43,14 +33,24 @@ type Doc struct {
 }
 
 type Identifier struct {
-	Pos  int
-	Site int
+	Pos  int // position index
+	Site int // client id
 }
 
 type Character struct {
 	Position []Identifier
 	Lamport  int
 	Char     string
+}
+
+type OpItem struct {
+	Ops []Item
+}
+
+type Item struct {
+	Type      string
+	Character string
+	Position  []Identifier
 }
 
 var currentText string = ""
@@ -61,6 +61,7 @@ var userTextDir = make(map[int]string)
 var maxInt = math.MaxInt64
 
 var doc Doc
+var clientIds = make([]string, 0)
 
 // can refactor away, currently working..
 func loadPage() (*Page, error) {
@@ -74,11 +75,11 @@ func initBody() {
 		Char:    ""}
 	beg.Position[0] = Identifier{Pos: 1, Site: -1}
 
-	end := Character{Position: make([]Identifier, 1),
-		Lamport: -1,
-		Char:    ""}
-	// this has to be changed to math.MaxInt32 if run on a 32 bit system
-	end.Position[0] = Identifier{Pos: int(maxInt), Site: -1}
+	// end := Character{Position: make([]Identifier, 1),
+	// 	Lamport: -1,
+	// 	Char:    ""}
+	// // this has to be changed to math.MaxInt32 if run on a 32 bit system
+	// end.Position[0] = Identifier{Pos: int(maxInt), Site: -1}
 
 	doc.Body[0] = beg
 	// doc.Body[1] = end // not sure if we need an end.
@@ -110,7 +111,7 @@ func comparePosition(id1 []Identifier, id2 []Identifier) int {
 	for i := 0; i < min(len(id1), len(id2)); i++ {
 		idComp := compareIdentifier(id1[i], id2[i])
 		if idComp != 0 {
-			return idComp
+			return idComp // 0 is eq, -1 is leq, 1 is geq
 		}
 	}
 
@@ -332,12 +333,22 @@ func genPos(pos1 []Identifier, pos2 []Identifier, site int) []Identifier {
 // 	http.Redirect(w, r, "/editor", http.StatusFound)
 // }
 
+func searchPosition(prevPosition []Identifier) int {
+	for i := 0; i < len(doc.Body); i++ {
+		if comparePosition(doc.Body[i].Position, prevPosition) == 0 {
+			return i
+		}
+	}
+	return -1
+}
+
 func main() {
 	/*	http.HandleFunc("/", homeHandler)
 		/	http.HandleFunc("/editor", editorHandler)
 		/	http.HandleFunc("/save", saveHandler)
 		/	log.Fatal(http.ListenAndServe(":8080", nil)) */
 	var curContent = ""
+	var uidPos = 1
 	/*	var curContent = map[string]interface{}{
 		"ops": map[string]interface{}{},
 	} */
@@ -351,6 +362,7 @@ func main() {
 		s.Emit("initContent", curContent)
 		s.Emit("crdtTransfer", doc)
 		s.Emit("initID", s.ID())
+		clientIds = append(clientIds, []string{s.ID()}...)
 
 		s.Join("bcast")
 		s.Join(s.ID()) // Joins room with its own ID in order for server to send client specific messages
@@ -359,6 +371,12 @@ func main() {
 
 	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
 		fmt.Println("disconnected user with id", s.ID(), " because: ", reason)
+		for i, v := range clientIds { // delete from clientIDs upon disconnect
+			if v == s.ID() {
+				clientIds = append(clientIds[:i], clientIds[i+1:]...)
+				break
+			}
+		}
 	})
 
 	// Server receives a new string
@@ -392,9 +410,111 @@ func main() {
 
 	})
 
-	// When a Delete event is sent
-	server.OnEvent("/", "Delete", func(s socketio.Conn, dltItem string) {
-		fmt.Println("Received Delete for: ", dltItem)
+	// TODO:
+	// When a Operation event is sent
+	server.OnEvent("/", "Operation", func(s socketio.Conn, opItem string) {
+		// opItem - either a delete or insert or delete and insert
+		// apply operation to server CRDT
+		fmt.Println("Received Operation for: ", opItem)
+		/*
+			{
+				"ops" : [
+						{
+							"Type" : "Insert",
+							"Character" : "A",
+							"Position" : []Position <- the previous position
+						},
+						{
+							"Type" : "Delete",
+							"Position" : []Position <- uid of deleted character
+						}
+					]
+			}
+		*/
+
+		// broadcast to room -> use an arr of client IDs and send only to non-writer client.
+		// there is a data structure, clientIds which is an array of IDs, just do
+		// server.BroadcastToRoom("", clientIds[i], "dirMsg", "HI")
+		a := []byte(opItem)
+		var anOpItem OpItem
+		if err := json.Unmarshal(a, &anOpItem); err != nil {
+			panic(err)
+		}
+
+		for i := 0; i < len(anOpItem.Ops); i++ {
+			if anOpItem.Ops[i].Type == "Insert" {
+				fmt.Println("Inserting")
+				prevPosition := anOpItem.Ops[i].Position
+				characterIdx := searchPosition(prevPosition)
+				prevPos := prevPosition[len(prevPosition)-1].Pos
+				prevSite := prevPosition[len(prevPosition)-1].Site
+				tempID, _ := strconv.Atoi(s.ID())
+				/*
+					if(prevPosition[prevPosition.length -1].Site = myID) { // last character was mine
+						uidPos+=1
+						prevPosition[prevPosition.length -1].Pos = uidPos
+					} else {
+						uidPos = 1
+						prevPosition.push({  // insert @ (retain+1) index (+1 for "" in beginning)
+						...{"Pos" : uidPos},
+						...{"Site" : myID}
+						})
+					}
+				*/
+				fmt.Println("found char indx:", characterIdx)
+				var curPosition []Identifier
+				if prevSite == tempID {
+					uidPos++
+					prevPosition[len(prevPosition)-1].Pos = uidPos
+					curPosition = prevPosition
+				} else if prevSite == -1 {
+					uidPos++
+					curPosition = append(prevPosition, Identifier{Pos: uidPos, Site: tempID})
+				} else {
+					uidPos = 1
+					curPosition = append(prevPosition, Identifier{Pos: uidPos, Site: tempID})
+				}
+				// curPosition := append(prevPosition, Identifier{Pos: prevPos + 1, Site: tempID})
+				fmt.Println("prevPos and Site: ", prevPos, prevSite)
+				var newChar = Character{Position: curPosition,
+					Lamport: -1,
+					Char:    anOpItem.Ops[i].Character}
+				fmt.Println("New character: ", newChar)
+				/*
+					arr1 = append(arr1, 0) // Making space for the new element
+					copy(arr1[3:], arr1[2:]) // Shifting elements
+					arr1[2] = 99 // Copying/inserting the value
+				*/
+				var temp = make([]Character, len(doc.Body[characterIdx+1:]))
+				copy(temp, doc.Body[characterIdx+1:])
+				doc.Body = append(doc.Body[:characterIdx+1], []Character{newChar}...)
+				doc.Body = append(doc.Body, temp...)
+			}
+		}
+
+		for i := 0; i < len(anOpItem.Ops); i++ {
+			if anOpItem.Ops[i].Type == "Delete" {
+				fmt.Println("Deleting")
+				prevPosition := anOpItem.Ops[i].Position
+				characterIdx := searchPosition(prevPosition)
+
+				doc.Body = append(doc.Body[:characterIdx], doc.Body[characterIdx+1:]...)
+			}
+		}
+		out, err := json.Marshal(doc)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("After operation: ", string(out))
+
+		for i := 0; i < len(clientIds); i++ {
+			fmt.Println("Is ", clientIds[i], " equal to ", s.ID())
+			if clientIds[i] != s.ID() {
+				fmt.Println("Broadcasting to: ", clientIds[i])
+				server.BroadcastToRoom("", clientIds[i], "crdtTransfer", string(out))
+			}
+		}
+
 	})
 
 	// Server receives a new sequence action (insert, delete, or insert and delete)
@@ -414,20 +534,6 @@ func main() {
 			fmt.Println()
 		}
 		// fmt.Println(oneHistory)
-
-		// var op Operation
-		// for i := 0; i < len(data); i++ {
-		// 	if ret, exists := data[i]["retain"]; exists {
-		// 		op.Retain = ret.(int)
-		// 	}
-		// 	if del, exists := data[i]["delete"]; exists {
-		// 		op.Delete = del.(int)
-		// 	}
-		// 	if ins, exists := data[i]["retain"]; exists {
-		// 		op.Insert = ins.(string)
-		// 	}
-
-		// }
 
 		// fmt.Println(op)
 
